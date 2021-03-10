56a48897e2a8e7bc556aa11dd7d04ec182894e49
OlegJust <70070354+OlegJust@users.noreply.github.com>
2021-03-10 22:28:58 +0300

    Add files via upload

--- shared_files/js/post.js
+++ shared_files/js/post.js
@@ -1,26 +1,79 @@
-!(function (a) {
-  'use strict'
-  'function' == typeof define && define.amd
-    ? define(['jquery'], a)
-    : 'undefined' != typeof exports
-    ? (module.exports = a(require('jquery')))
-    : a(jQuery)
-})(function (a) {
-    (a.fn.slick = function () {
-      var f,
-        g,
-        a = this,
-        c = arguments[0],
-        d = Array.prototype.slice.call(arguments, 1),
-        e = a.length
-      for (f = 0; e > f; f++)
-        if (
-          ('object' == typeof c || 'undefined' == typeof c
-            ? (a[f].slick = new b(a[f], c))
-            : (g = a[f].slick[c].apply(a[f].slick, d)),
-          'undefined' != typeof g)
-        )
-          return g
-      return a
-    })
+const callForm = document.querySelector('#callForm')
+const kmacbForm = document.querySelector('#kmacb-form')
+const kmf = document.querySelector('#kmf')
+const kmf_Flex = document.querySelector('#kmacb-form-flex')
+const grid__item1 = document.querySelector('.grid__item1')
+const grid__item2 = document.querySelector('.grid__item2')
+const grid__item3 = document.querySelector('.grid__item3')
+const grid__item4 = document.querySelector('.grid__item4')
+const grid__item5 = document.querySelector('.grid__item5')
+const iconClose = document.querySelector('.icon-close')
+const kmacbcli = document.querySelector('#kmacbcli')
+const html  = document.querySelector('html')
+const body  = document.querySelector('body')
+kmf.style.top = `0px`
+kmf.style.left = `0px`
+
+
+function disableScrolling(){
+	var x=window.scrollX;
+	var y=window.scrollY;
+	window.onscroll=function(){window.scrollTo(x, y);};
+}
+
+function enableScrolling(){
+	window.onscroll=function(){};
+}
+
+
+callForm.addEventListener('click', (event) => {
+  // kmacbForm.style.display = 'block'
+	kmacbForm.style.display = 'grid'
+	kmacbForm.style.gridTemplate = "1fr 550px 1fr / 1fr 460px 1fr"
+	kmacbForm.style.gridTemplateAreas = "'header header header' 'block-1 block-2 block-3' 'footer footer footer'"
+	disableScrolling()
+	body.style.paddingRight= "17px"
+	html.style.overflow= "hidden";
+})
+
+
+kmacbcli.addEventListener('click', (event) => {
+	kmacbForm.style.display = 'grid'
+	kmacbForm.style.gridTemplate = "1fr 550px 1fr / 1fr 460px 1fr"
+	kmacbForm.style.gridTemplateAreas = "'header header header' 'block-1 block-2 block-3' 'footer footer footer'"
+	disableScrolling()
+	body.style.paddingRight= "17px"
+	html.style.overflow= "hidden";
+})
+//position: relative;
+//z-index: 2;
+grid__item1.addEventListener('click', (event) => {
+	enableScrolling()
+	body.style.paddingRight= "0px"
+	html.style.overflow= "auto ";
+	kmacbForm.style.display = 'none'
+})
+grid__item2.addEventListener('click', (event) => {
+	body.style.paddingRight= "0px"
+	enableScrolling()
+	html.style.overflow= "auto ";
+	kmacbForm.style.display = 'none'
+})
+grid__item4.addEventListener('click', (event) => {
+	body.style.paddingRight= "0px"
+	enableScrolling()
+	html.style.overflow= "auto ";
+	kmacbForm.style.display = 'none'
+})
+grid__item5.addEventListener('click', (event) => {
+	body.style.paddingRight= "0px"
+	enableScrolling()
+	html.style.overflow= "auto ";
+	kmacbForm.style.display = 'none'
+})
+iconClose.addEventListener('click', (event) => {
+	body.style.paddingRight= "0px"
+	enableScrolling()
+	html.style.overflow= "auto ";
+	kmacbForm.style.display = 'none'
 })
